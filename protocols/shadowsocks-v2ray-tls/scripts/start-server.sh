6e1e7b856ed890b1e9680fb831ab0cd53d1a2c26
0xland <98829546+0xland@users.noreply.github.com>
2022-12-23 14:39:10 +0000

    set android uri mux to 16

--- protocols/shadowsocks-v2ray-tls/scripts/start-server.sh
+++ protocols/shadowsocks-v2ray-tls/scripts/start-server.sh
@@ -9,7 +9,7 @@ ARGS="--plugin v2ray-plugin --plugin-opts server;fast-open;path=${URL_PATH} -u"
 android_url () {
     encoded_M_P=$(echo -n "$METHOD:$PASSWORD" | base64);
     encoded_M_P=$(echo "$encoded_M_P" | sed -e "s/=//g")
-    ANDROID_URL="ss://$encoded_M_P@$DOMAIN:443?plugin=v2ray-plugin%3Bpath%3D%2F${V2RAY_PATH}%3Bmux%3D2%3Bloglevel%3Dnone%3Bhost%3D$DOMAIN%3Btls"
+    ANDROID_URL="ss://$encoded_M_P@$DOMAIN:443?plugin=v2ray-plugin%3Bpath%3D%2F${V2RAY_PATH}%3Bmux%3D16%3Bloglevel%3Dnone%3Bhost%3D$DOMAIN%3Btls"
 }
 ios_url () {
     ios_encoded_M_P=$(echo -n "$METHOD:$PASSWORD@$DOMAIN:443" | base64);
